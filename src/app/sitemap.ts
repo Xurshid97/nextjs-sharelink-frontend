7141b2af7c04103adfb999c0285696c51ef5eb90
Xurshid97 <129675148+Xurshid97@users.noreply.github.com>
2024-07-29 17:44:36 +0200

    SEO global page routing is made

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,19 +1,28 @@
-import { MetadataRoute } from 'next'
+import { MetadataRoute } from 'next';
 import { getGlobalCategories } from './api/categoriesAPI';
 
 // Mock function to fetch dynamic routes
 // Replace this with your actual implementation
 async function fetchDynamicRoutes() {
     const globalCategories = await getGlobalCategories();
-    const dynamicRoutes = globalCategories.map((category: { username: string, globalcategory: string, name: string }) => `/global/${category.username}/${category.globalcategory}/${category.name}`);
-    // Example dynamic routes
-    return dynamicRoutes
+
+    // Check if globalCategories is an array
+    if (!Array.isArray(globalCategories)) {
+        console.error('getGlobalCategories did not return an array', globalCategories);
+        throw new TypeError('Expected getGlobalCategories to return an array');
+    }
+
+    const dynamicRoutes = globalCategories.map((category: { username: string, globalcategory: string, name: string }) =>
+        `/global/${category.username}/${category.globalcategory}/${category.name}`
+    );
+
+    return dynamicRoutes;
 }
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-    const dynamicRoutes = await fetchDynamicRoutes()
+    const dynamicRoutes = await fetchDynamicRoutes();
 
-    const staticRoutes = [
+    const staticRoutes: MetadataRoute.Sitemap = [
         {
             url: 'https://sharelinc.store',
             lastModified: new Date(),
@@ -26,17 +35,17 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
             changeFrequency: 'daily',
             priority: 1,
         },
-    ]
+    ];
 
-    const dynamicSitemapEntries = dynamicRoutes.map((route: string) => ({
+    const dynamicSitemapEntries: MetadataRoute.Sitemap = dynamicRoutes.map((route: string) => ({
         url: `https://sharelinc.store${route}`,
         lastModified: new Date(),
-        changeFrequency: 'hourly',
+        changeFrequency: 'hourly', // This value is allowed
         priority: 1,
-    }))
+    }));
 
     return [
         ...staticRoutes,
         ...dynamicSitemapEntries
-    ]
+    ];
 }
