8ed8c7d5e18623f682415f94ee21be3ebf20a2e9
Xurshid97 <xurshedrabo@gmail.com>
2024-10-03 21:04:37 +0200

    layout server comonent

--- src/app/global/PublicCategoriesList.tsx
+++ src/app/global/PublicCategoriesList.tsx
@@ -1,15 +1,9 @@
-"use client"
-import { useState, useEffect } from "react";
+
 import {
     Card,
     Skeleton,
-    Typography,
-    Divider,
-    List,
 } from "antd";
 import Link from "next/link";
-import InfiniteScroll from "react-infinite-scroll-component";
-const Text = Typography.Text;
 
 type Category = {
     id: number;
@@ -32,85 +26,76 @@ export default function PublicCategories({ categories }: { categories: Category[
             }}>
             {
                 loading ?
-                    <List
-                        dataSource={[1, 2, 3, 4, 5]}
-                        renderItem={(num) => (
+                    <ul
+                        style={{
+                            listStyleType: "none",
+                            padding: "0",
+                            margin: "0",
+                            width: "100%",
+                            display: "flex",
+                            flexWrap: "wrap",
+                            gap: "16px",
+                        }}>
+                        <Card
+                            className="category-card"
+                            style={{
+                                marginBottom: "16px",
+                            }}>
+                            <Skeleton
+                                loading={loading}
+                                avatar
+                                active>
+                                <p
+                                    style={{
+                                        color: "blue",
+                                        fontWeight: "bold",
+                                    }}>
+                                    Category Name
+                                </p>
+                            </Skeleton>
+                        </Card>
+                    </ul>
+                    :
+                    <ul
+                    style={{
+                        listStyleType: "none",
+                        padding: "0",
+                        margin: "0",
+                        width: "100%",
+                        display: "flex",
+                        flexDirection: "column",
+                        flexWrap: "wrap",
+                        gap: "16px",
+                    }}
+                >
+                    {
+                        categories.map((category) => (
                             <Card
-                                key={num}
-                                className="category-card">
-                                <br />
-                                <Skeleton
-                                    loading={loading}
-                                    avatar
-                                    active>
-                                    <Link
-                                        href={`${num
-                                            }`}>
-                                        <Text
-                                            style={{
-                                                color: "blue",
-                                                fontWeight: "bold",
-                                            }}>
-                                            {num}
-                                        </Text>
-                                    </Link>
-                                </Skeleton>
-                            </Card>
-                        )}
-                    /> :
-                    // <InfiniteScroll
-                    //     dataLength={categories && categories.length}
-                    //     next={loadMoreData}
-                    //     hasMore={categories.length < 0}
-                    //     loader={
-                    //         <Skeleton
-                    //             avatar
-                    //             paragraph={{ rows: 1 }}
-                    //             active
-                    //         />
-                    //     }
-                    //     endMessage={
-                    //         <Divider plain>It is all, nothing more 🤐</Divider>
-                    //     }
-                    //     scrollableTarget="scrollableDiv">
-                        <List
-                            dataSource={categories}
-                            renderItem={(category: Category) => (
-                                <Card
                                     key={category.id}
                                     className="category-card"
                                     style={{
                                         marginBottom: "16px",
                                     }}>
-                                    {/* <Text
-                                style={{
-                                    fontWeight: "bold",
-                                }}>
-                                Created by:{" "}
-                                {(category as { username?: string }).username
-                                    ? (category as { username?: string })
-                                          .username
-                                    : "Anonymous"}
-                            </Text> */}
                                     <Skeleton
                                         loading={loading}
                                         avatar
                                         active>
                                         <Link
                                             href={`global/${(category as { username: string }).username}/${(category as { globalcategory: string }).globalcategory}/${(category as { name: string }).name}`}>
-                                            <Text
+                                            <p
                                                 style={{
                                                     color: "blue",
                                                     fontWeight: "bold",
                                                 }}>
                                                 {(category as { name: string }).name}
-                                            </Text>
+                                            </p>
                                         </Link>
                                     </Skeleton>
                                 </Card>
-                            )}
-                        />
-                    // </InfiniteScroll>
+                        )
+                        )
+                        }
+                    </ul>
             }
         </div>
     );
--- src/app/global/[companyName]/[globalCategoryName]/[category_name]/GlobalLinksList.tsx
+++ src/app/global/[companyName]/[globalCategoryName]/[category_name]/GlobalLinksList.tsx
@@ -1,17 +1,13 @@
-"use client"
 import { changed_img } from "../../../../constants/urls";
 import Link from "next/link";
 import {
     Avatar,
     Card,
-    Divider,
     Flex,
-    List,
     Skeleton,
 } from "antd";
 import Meta from "antd/es/card/Meta";
 import { ShareAltOutlined, ExportOutlined } from "@ant-design/icons";
-import InfiniteScroll from "react-infinite-scroll-component";
 import styles from './styles.module.css'
 
 interface LinkData {
@@ -63,102 +59,94 @@ function Links({ links }: { links: LinkData[] }) {
                         />
                     </Flex>
                 ) : (
-                    <InfiniteScroll
-                        dataLength={links.length}
-                        next={fetchData}
-                        hasMore={false}
-                        loader={
-                            <Skeleton
-                                avatar
-                                paragraph={{ rows: 1 }}
-                                active
-                            />
-                        }
-                        endMessage={
-                            <Divider plain>It is all, nothing more 🤐</Divider>
-                        }
-                        scrollableTarget="scrollableDiv">
-                        <List
-                            className={styles.cardsParent}
-                            dataSource={links}
-                            renderItem={(link) => (
+                        <ul
+                            style={{
+                                listStyleType: "none",
+                                padding: "0",
+                                margin: "0",
+                                width: "100%",
+                                display: "flex",
+                                flexWrap: "wrap",
+                                gap: "16px",
+                            }}
+                        >
+                            {links.map((link) => (
                                 <Card
-                                    className={styles.cardLinks}
-                                    cover={
-                                        link.image ? (
-                                            <Avatar
-                                                size={{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80, xxl: 100 }}
-                                                style={{
-                                                    width: "100%",
-                                                    border: "none",
-                                                    height: "100%", // Ensure the image takes full height
-                                                    padding: "5px",
-                                                    backgroundColor: "transparent",
-                                                }}
-                                                icon={
-                                                    <img
-                                                        src={changed_img + link.image ? changed_img + link.image : "#"}
-                                                        alt={link.title}
-                                                        style={{
-                                                            width: "100%",
-                                                            height: "100%",  // Ensure the image takes full height
-                                                            objectFit: "cover", // This will make sure the image covers the Avatar area without distortion
-                                                            borderRadius: "8px",
-                                                        }}
-                                                    />}
-                                            />
-                                        ) : (
-                                            <ShareAltOutlined />
-                                        )
-                                    }
-                                    key={link.id}>
-                                    <div className="link-details">
-                                        <Card
+                                className={styles.cardLinks}
+                                cover={
+                                    link.image ? (
+                                        <Avatar
+                                            size={{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80, xxl: 100 }}
                                             style={{
-                                                border: "none",
                                                 width: "100%",
-                                            }}>
-                                            <Meta
-                                                title={
-                                                    <Link
-                                                        href={`${link.url}`}
-                                                        target={"_blank"}
-                                                        rel="noopener noreferrer">
-                                                        <div
-                                                            style={{
-                                                                display: "inline-block",
-                                                                whiteSpace: "normal",
-                                                                wordWrap: "break-word",
-                                                            }}
-                                                        >
-                                                            {link.title}
-                                                            <ExportOutlined style={{
-                                                                marginLeft: "8px",
-                                                            }} />
-                                                        </div>
+                                                border: "none",
+                                                height: "100%", // Ensure the image takes full height
+                                                padding: "5px",
+                                                backgroundColor: "transparent",
+                                            }}
+                                            icon={
+                                                <img
+                                                    src={changed_img + link.image ? changed_img + link.image : "#"}
+                                                    alt={link.title}
+                                                    style={{
+                                                        width: "100%",
+                                                        height: "100%",  // Ensure the image takes full height
+                                                        objectFit: "cover", // This will make sure the image covers the Avatar area without distortion
+                                                        borderRadius: "8px",
+                                                    }}
+                                                />}
+                                        />
+                                    ) : (
+                                        <ShareAltOutlined />
+                                    )
+                                }
+                                key={link.id}>
+                                <div className="link-details">
+                                    <Card
+                                        style={{
+                                            border: "none",
+                                            width: "100%",
+                                        }}>
+                                        <Meta
+                                            title={
+                                                <Link
+                                                    href={`${link.url}`}
+                                                    target={"_blank"}
+                                                    rel="noopener noreferrer">
+                                                    <div
+                                                        style={{
+                                                            display: "inline-block",
+                                                            whiteSpace: "normal",
+                                                            wordWrap: "break-word",
+                                                        }}
+                                                    >
+                                                        {link.title}
+                                                        <ExportOutlined style={{
+                                                            marginLeft: "8px",
+                                                        }} />
+                                                    </div>
 
-                                                        <p
-                                                            style={{
-                                                                fontSize: "12px",
-                                                                color: "gray",
-                                                                marginTop: "1px",
-                                                                marginBottom: "1px",
-                                                            }}>
-                                                            {link.url.split("/")[2] === 't.me' ?
-                                                                link.url.split("//")[1] :
-                                                                link.url.split("//")[0] + "//" + link.url.split("/")[2]
-                                                            }
-                                                        </p>
-                                                    </Link>
-                                                }
-                                                description={link.description}
-                                            />
-                                        </Card>
-                                    </div>
-                                </Card>
-                            )}
-                        />
-                    </InfiniteScroll>
+                                                    <p
+                                                        style={{
+                                                            fontSize: "12px",
+                                                            color: "gray",
+                                                            marginTop: "1px",
+                                                            marginBottom: "1px",
+                                                        }}>
+                                                        {link.url.split("/")[2] === 't.me' ?
+                                                            link.url.split("//")[1] :
+                                                            link.url.split("//")[0] + "//" + link.url.split("/")[2]
+                                                        }
+                                                    </p>
+                                                </Link>
+                                            }
+                                            description={link.description}
+                                        />
+                                    </Card>
+                                </div>
+                            </Card>
+                            ))}
+                        </ul>
                 )
                 }
             </div >
